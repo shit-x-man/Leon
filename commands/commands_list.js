3464c33b80248a56e67037619a750ed316211cc3
someShit <72192832+shit-x-man@users.noreply.github.com>
2024-01-25 11:20:58 +0530

    Update commands_list.js

--- commands/commands_list.js
+++ commands/commands_list.js
@@ -1,13 +1,16 @@
 const command = require('../helpers/database/commands');
 
 module.exports = {
-  command: 'commands',
+  command: 'excom',
   info: 'Shows installed external commands list.',
   func: async (sock, msg, text) => {
     let list = '*Installed external commands are:*\n\n';
     let l = await command.list();
     if (l.length < 1) return await msg.reply({ text: '*There is no external command installed!*' });
-    list += l.map(async (command) => `*${command.name}* : ${command.url}`).join('\n\n');
+    const commandList = await Promise.all(
+      l.map(async (command) => `*${command.name}* : ${command.url}`)
+    );
+    list += commandList.join('\n\n');
     return await msg.reply({ text: list });
   }
 };
