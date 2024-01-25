66f7da66a1e7089d2d966ea188763b1d47f0e369
someShit <72192832+shit-x-man@users.noreply.github.com>
2024-01-25 10:17:18 +0530

    Update commands_list.js

--- commands/commands_list.js
+++ commands/commands_list.js
@@ -7,7 +7,7 @@ module.exports = {
     let list = '*Installed external commands are:*\n\n';
     let l = await command.list();
     if (l.length < 1) return await msg.reply({ text: '*There is no external command installed!*' });
-    list += l.forEach(async (command) => '*' + command.name + '* : ' + command.url).join('\n\n');
+    list += l.map(async (command) => `*${command.name}* : ${command.url}`).join('\n\n');
     return await msg.reply({ text: list });
   }
 };
